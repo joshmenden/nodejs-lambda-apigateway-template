0a9129b1394b6894788036f6a3678ffa9ff347bc
Josh Menden <josh.menden@gmail.com>
2020-08-22 16:15:47 -0600

    Base lots of project of project-name.txt

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -9,15 +9,15 @@ FUNCTION=$(aws cloudformation describe-stack-resource --stack-name $STACK --logi
 aws cloudformation delete-stack --stack-name $STACK
 echo "Deleted $STACK stack."
 
-if [ -f bucket-name.txt ]; then
-    ARTIFACT_BUCKET=$(cat bucket-name.txt)
+if [ -f project-name.txt ]; then
+    ARTIFACT_BUCKET=$(cat project-name.txt)
     if [[ ! $ARTIFACT_BUCKET =~ lambda-artifacts-[a-z0-9]{16} ]] ; then
         echo "Bucket was not created by this application. Skipping."
     else
         while true; do
             read -p "Delete deployment artifacts and bucket ($ARTIFACT_BUCKET)? (y/n)" response
             case $response in
-                [Yy]* ) aws s3 rb --force s3://$ARTIFACT_BUCKET; rm bucket-name.txt; break;;
+                [Yy]* ) aws s3 rb --force s3://$ARTIFACT_BUCKET; rm project-name.txt; break;;
                 [Nn]* ) break;;
                 * ) echo "Response must start with y or n.";;
             esac
--- scripts/create-bucket.sh
+++ scripts/create-bucket.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-BUCKET_ID=$(dd if=/dev/random bs=8 count=1 2>/dev/null | od -An -tx1 | tr -d ' \t\n')
-BUCKET_NAME=lambda-artifacts-$BUCKET_ID
-echo $BUCKET_NAME > bucket-name.txt
-aws s3 mb s3://$BUCKET_NAME
+PROJECT_NAME=$(cat project-name.txt)
+aws s3 mb s3://$PROJECT_NAME
