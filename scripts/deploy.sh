c90d79c60ee57c1fc16dc438ed46bec78375545a
Josh Menden <josh.menden@gmail.com>
2020-08-27 18:16:09 -0600

    Support babel and node modules in template

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -eo pipefail
 PROJECT_NAME=$(cat project-name.txt)
+cd src
+babel . -d ../dist --ignore node_modules
+cp -r ./node_modules ../dist/
+cd ..
 aws cloudformation package --template-file template.yml --s3-bucket $PROJECT_NAME --output-template-file out.yml
 aws cloudformation deploy --template-file out.yml --stack-name $PROJECT_NAME --capabilities CAPABILITY_NAMED_IAM
+rm -rf ./dist
--- scripts/get.sh
+++ scripts/get.sh
@@ -3,5 +3,5 @@ set -eo pipefail
 PROJECT_NAME=$(cat project-name.txt)
 APIID=$(aws cloudformation describe-stack-resource --stack-name $PROJECT_NAME --logical-resource-id api --query 'StackResourceDetail.PhysicalResourceId' --output text)
 REGION=$(aws configure get region)
-
+echo $https://$APIID.execute-api.$REGION.amazonaws.com/api/
 curl https://$APIID.execute-api.$REGION.amazonaws.com/api/ -v
